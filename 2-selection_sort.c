d640b56efe6a55abf990b4b12ae7a8ab994763d0
thomas rousseau <thomas.rousseau19@gmail.com>
2025-04-11 17:55:59 +0200

    betty style task 2 et 3

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,29 +6,29 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    int index = 0, min, tmp;
-    size_t index1, index2;
+int index = 0, min, tmp;
+size_t index1, index2;
 
-    if (array == NULL || size < 2)
-        return;
+if (array == NULL || size < 2)
+return;
 
-    for (index1 = 0; index1 < size - 1; index1++)
-    {
-        min = array[index1];
-        for (index2 = index1 + 1; index2 < size; index2++)
-        {
-            if (array[index2] < min)
-            {
-                min = array[index2];
-                index = index2;
-            }
-        }
-        if (min < array[index1])
-        {
-            tmp = array[index1];
-            array[index1] = array[index];
-            array[index] = tmp;
-            print_array(array, size);
-        }
-    }
+for (index1 = 0; index1 < size - 1; index1++)
+{
+min = array[index1];
+for (index2 = index1 + 1; index2 < size; index2++)
+{
+if (array[index2] < min)
+{
+min = array[index2];
+index = index2;
+}
+}
+if (min < array[index1])
+{
+tmp = array[index1];
+array[index1] = array[index];
+array[index] = tmp;
+print_array(array, size);
+}
+}
 }
--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,34 +10,34 @@
  */
 int partition(int *array, int low, int high, int size)
 {
-	int pivot = array[high];
-	int i = low;
-	int j, temp;
+int pivot = array[high];
+int i = low;
+int j, temp;
 
-	for (j = low; j < high; j++)
-	{
-		if (array[j] <= pivot)
-		{
-			if (i != j)
-			{
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-				print_array(array, size);
-			}
-			i++;
-		}
-	}
+for (j = low; j < high; j++)
+{
+if (array[j] <= pivot)
+{
+if (i != j)
+{
+temp = array[i];
+array[i] = array[j];
+array[j] = temp;
+print_array(array, size);
+}
+i++;
+}
+}
 
-	if (i != high)
-	{
-		temp = array[i];
-		array[i] = array[high];
-		array[high] = temp;
-		print_array(array, size);
-	}
+if (i != high)
+{
+temp = array[i];
+array[i] = array[high];
+array[high] = temp;
+print_array(array, size);
+}
 
-	return (i);
+return (i);
 }
 
 /**
@@ -49,12 +49,12 @@ int partition(int *array, int low, int high, int size)
  */
 void quicksort_recursive(int *array, int low, int high, int size)
 {
-	if (low < high)
-	{
-		int pivot_index = partition(array, low, high, size);
-		quicksort_recursive(array, low, pivot_index - 1, size);
-		quicksort_recursive(array, pivot_index + 1, high, size);
-	}
+if (low < high)
+{
+int pivot_index = partition(array, low, high, size);
+quicksort_recursive(array, low, pivot_index - 1, size);
+quicksort_recursive(array, pivot_index + 1, high, size);
+}
 }
 
 /**
@@ -64,8 +64,8 @@ void quicksort_recursive(int *array, int low, int high, int size)
  */
 void quick_sort(int *array, size_t size)
 {
-	if (!array || size < 2)
-		return;
+if (!array || size < 2)
+return;
 
-	quicksort_recursive(array, 0, size - 1, size);
+quicksort_recursive(array, 0, size - 1, size);
 }
